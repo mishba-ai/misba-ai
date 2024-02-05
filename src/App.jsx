e16931934f65cbd572fc1779b920a8aefdf28a73
misbah <121809535+mishba-ai@users.noreply.github.com>
2024-02-05 05:03:10 +0000

    update the portfolio and remove unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
+  name: "MISHBA BAGBAN",
+  title: "FULLSTACK WEB-DEVELOPER & TECHNICAL WRITER",
   email: "alex@example.com",
-  gitHub: "microsoft",
+  gitHub: "mishba-ai",
   instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  linkedIn: "mishba",
+  Hashnode: "",
+  twitter: "mishba",
 };
 
 const primaryColor = "#4E567E";
@@ -42,7 +41,7 @@ const App = () => {
   return (
     <div id="main">
       <Header />
-      <Home name={siteProps.name} title={siteProps.title} />
+      <Home title={siteProps.title} />
       <About />
       <Portfolio />
       <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,3 @@
-/**
- * Header component
- *
- * Top navigation bar for your site. Set to remain visible as the
- * user scrolls so that they can constantly reach any part of your page.
- */
 import React from "react";
 
 const Header = () => {
@@ -13,18 +7,19 @@ const Header = () => {
         position: "fixed",
         display: "flex",
         justifyContent: "center",
-        gap: "2rem",
-        background: "rgba(255,255,255,0.75)",
+        gap: "3rem",
+        background: "transparent",
         padding: "1rem",
         top: 0,
         width: "100%",
         zIndex: 10,
+        fontFamily: 'Roboto Mono , monospace',
       }}
     >
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-      <a href="#portfolio">Portfolio</a>
-      <a href="#footer">Contact</a>
+      <a href="#home" style={{color:'white'}}>.HOME</a>
+      <a href="#about" style={{color:'white'}}>.ABOUT</a>
+      <a href="#portfolio" style={{color:'white'}}>.PORTFOLIO</a>
+      <a href="#footer" style={{color:'white'}}>.CONTACT</a>
     </div>
   );
 };
