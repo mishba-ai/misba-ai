ad2345b862cc560a9a3fdf43e2e4c1667c38dd53
mishba-ai <mishbabagban@gmail.com>
2024-09-18 18:35:29 +0530

    add the image in the contact-me page

--- src/pages/Contactme.jsx
+++ src/pages/Contactme.jsx
@@ -4,7 +4,13 @@ export default function Contactme() {
   return (
     <div>
       <h1 className="font-Kode text-violet-200 text-2xl font-medium "> Work with Me </h1>
-
+      <div>
+        <img
+          src="https://i.pinimg.com/564x/4a/c4/55/4ac4550a21c8b74b253e2d7503200fae.jpg"
+          className="mt-4 rounded-lg opacity-30"
+          alt=""
+        />
+      </div>
       <div className="font-code text-xl font-medium">
         <p className="mt-4">I am open to full-time opportunities</p>
         <div className="flex gap-x-2 mt-2">
